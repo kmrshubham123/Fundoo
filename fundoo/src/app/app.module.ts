2304721eba90f22a9f1673280e08bcb1e10938f1
kmrshubham123 <kmrshubham123@gmail.com>
2021-09-16 21:56:58 +0530

    [Kumar Shubham]Added:created Login UI

--- fundoo/src/app/app.module.ts
+++ fundoo/src/app/app.module.ts
@@ -5,19 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    LoginComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,MatFormFieldModule,MatButtonModule
+    BrowserAnimationsModule,MatFormFieldModule,MatButtonModule,MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
