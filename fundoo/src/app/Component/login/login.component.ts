aa531b4b261b60e56da6b20177a02dbe81b748e2
kmrshubham123 <kmrshubham123@gmail.com>
2021-09-21 15:17:42 +0530

    [Kumar Shubham]Refactor:http and user services calling in login-component

--- fundoo/src/app/Component/login/login.component.ts
+++ fundoo/src/app/Component/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../../services/userService/user.service';
 
 @Component({
   selector: 'app-login',
@@ -8,18 +9,37 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  loginForm! : FormGroup;
+  loginForm!: FormGroup;
 
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private userService: UserService) { }
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
- 
+
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
 
     });
+  }
+
+  onSubmit() {
+    console.log("onsubmit function is calling", this.loginForm.value);
+    let request = {
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password,
+
+    }
+    console.log(request)
+    this.userService.loginUser(request).subscribe((response: any) => {
+      console.log(response)
+
+
+
+    }, (error: any) => {
+      console.log(error)
+
+    })
   }
 
   // convenience getter for easy access to form fields
