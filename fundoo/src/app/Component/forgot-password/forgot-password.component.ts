f54fbd5d01df9a236cd3ad930a96bfbba6a1b1b7
kmrshubham123 <kmrshubham123@gmail.com>
2021-09-21 15:14:03 +0530

    [Kumar Shubham]Refactor:http and user services calling in forgot-password- component

--- fundoo/src/app/Component/forgot-password/forgot-password.component.ts
+++ fundoo/src/app/Component/forgot-password/forgot-password.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { UserService } from '../../services/userService/user.service';
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
@@ -8,17 +8,35 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  forgotPasswordForm! : FormGroup;
+  forgotPasswordForm!: FormGroup;
 
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private userService: UserService) { }
 
   ngOnInit() {
     this.forgotPasswordForm = this.formBuilder.group({
 
       email: ['', [Validators.required, Validators.email]]
-      
+
     });
+  }
+
+  onSubmit() {
+    console.log("onsubmit function is calling  ", this.forgotPasswordForm.value);
+    let request = {
+
+      email: this.forgotPasswordForm.value.email,
+
+
+    }
+    console.log(request)
+    this.userService.forgotUser(request).subscribe((response: any) => {
+      console.log(response);
+
+    }, (error: any) => {
+      console.log(error);
+
+    })
   }
 
   // convenience getter for easy access to form fields
