09651b5069afca4319ee0dbcc28a4b140dcdf949
kmrshubham123 <kmrshubham123@gmail.com>
2021-09-24 14:21:44 +0530

    [Kumar Shubham]Added:create display component

---
+++ fundoo/src/app/Component/display/display.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display',
+  templateUrl: './display.component.html',
+  styleUrls: ['./display.component.scss']
+})
+export class DisplayComponent implements OnInit {
+
+  constructor() { }
+  @Input() NoteArray: any;
+  
+
+  ngOnInit(){
+    console.log(this.NoteArray)
+  }
+
+}
