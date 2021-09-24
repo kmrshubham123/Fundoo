610b37afbc1660f0466d294255603065c8c5bb85
kmrshubham123 <kmrshubham123@gmail.com>
2021-09-24 14:27:50 +0530

    [Kumar Shubham]Added:create noteservice for takenote,display and getall notes

---
+++ fundoo/src/app/services/noteservice/note.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from '../httpService/http.service';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class NoteService {
+  BaseUrl=environment.BaseUrl
+token:any
+  constructor(private httpService:HttpService ) { 
+    this.token = localStorage.getItem('token')
+  }
+ 
+  // createNote( request:any){
+  //   this.httpService.Post('notes/addNotes',request)
+
+  // }
+
+
+  createNotes(data:any) : Observable<any>{
+    let httpAuthOptions = {
+      headers:new HttpHeaders({
+        'Content-Type':'application/json',
+        'Authorization': this.token
+      })
+    };
+    return this.httpService.PostService(this.BaseUrl + '/notes/addNotes',data, true, httpAuthOptions);
+
+  }
+
+
+  getAllNoteService(){
+    let httpAuthOptions = {
+      headers:new HttpHeaders({
+        'Content-Type':'application/json',
+        'Authorization': this.token
+      })
+    };
+    return this.httpService.GetService(this.BaseUrl + '/notes/getNotesList', true, httpAuthOptions);
+
+  }
+}
