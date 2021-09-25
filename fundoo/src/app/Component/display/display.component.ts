71596f5e90adabdd9a6b1bee277b5ca9e3adc3c7
kmrshubham123 <kmrshubham123@gmail.com>
2021-09-26 01:13:13 +0530

    [Kumar Shubham]Refactor: api integration and crud opertion

--- fundoo/src/app/Component/display/display.component.ts
+++ fundoo/src/app/Component/display/display.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { UpdatenoteComponent } from '../updatenote/updatenote.component';
 
 @Component({
   selector: 'app-display',
@@ -6,13 +8,25 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./display.component.scss']
 })
 export class DisplayComponent implements OnInit {
+  constructor(public dialog: MatDialog) {}
 
-  constructor() { }
   @Input() NoteArray: any;
   
 
   ngOnInit(){
     console.log(this.NoteArray)
+  }
+  
+  openDialog(notecard: any): void {
+    const dialogRef = this.dialog.open(UpdatenoteComponent, {
+      width: '350px',
+      data: notecard
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed',result);
+
+    });
   }
 
 }
